cff46c7c05947b64997ea5f21f7d012593f81bf6
ManiPrasadMP <maniprasadkr08@gmail.com>
2025-07-15 10:22:32 +0530

    sec

---
+++ DJ.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class DJ {
+	void dance(String name,String songN,double weight) {
+		System.out.println("Jay is dancing");
+	}
+	void dance(String name,double weight,String songN) {
+		System.out.println("Gopi is dancing");
+	}
+
+}
